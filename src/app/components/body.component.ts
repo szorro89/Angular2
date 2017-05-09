488f7cc230c1e5fbf3e631cd0cefa01df813e8a9
sergiozorro <sergiozorro@seven4n.com>
2017-05-09 11:31:20 -0500

    Primer commit del primer proyecto.

---
+++ src/app/components/body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-body',
+  templateUrl: 'body.component.html',
+})
+export class BodyComponent {
+
+  mostrar:boolean = true;
+
+  texto:string = "Un gran poder, requiere una gran resopnsabilidad.";
+  autor:string = "Ben Parker";
+
+  personajes:string[] = ["Spiderman", "Venom", "Dr. Octopus"];
+}
